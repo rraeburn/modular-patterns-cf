fb8962cebf305159364bd2b98ebcd7fc8de3bc15
Ryan Raeburn <ryanraeburn@gmail.com>
2015-02-04 11:43:45 -0800

    added .jshintrc file and grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,21 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     jshint: {
       dev: {
         options: {
-          node: true,
-          globals: {
-            describe: true,
-            it: true
-          }
+          jshintrc: '.jshintrc'
         },
         src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
       }
+    },
+
+    watch: {
+      files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js'],
+      tasks: ['default']
     },
 
     simplemocha: {
